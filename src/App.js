b3d9b35eacf0e5b3100c6f12da2ba8cf7ecb09e3
Luis Hurtado <luisferhc352@gmail.com>
2021-08-21 10:57:45 -0500

    component that render all the sub-components and its styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Main from './main/main';
+import AppContextProvider from "./context/AppContext.js"
+
+import './App.css';
+
+function App() {
+ 
+  return (
+        <AppContextProvider>
+         <Main/>
+        </AppContextProvider>
+  );
+}
+export default App;
