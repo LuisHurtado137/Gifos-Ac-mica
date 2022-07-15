cfd191fd05cc2243237c237224ae6e1eab726872
Luis Hurtado <luisferhc352@gmail.com>
2022-07-15 14:41:29 -0500

    new loader added

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import '../Loader/Loader.css'
+import React from "react";
+import "../Loader/Loader.css";
 
-export default function Loading()  {
-    return (
-        <div>
-            <div className="loading">
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>    
-            </div>
-            <h2 className="loading-title">Cargando tus GIFS</h2>
-        </div>
-    )
+export default function Loading() {
+  const LOADING = "Buscando tus gifs...";
+
+  return (
+    <>
+      {LOADING}
+      <br />
+      <span class="loader"></span>
+    </>
+  );
 }
